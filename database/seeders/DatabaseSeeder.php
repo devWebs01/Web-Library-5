513a3e4c6d39dd182621e270d156a26e232ce7ce
Adi Putra <m.a.syaputra.94@gmail.com>
2025-03-05 00:07:28 +0700

    fix: database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,8 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             StatusSeeder::class,
             // APISeeder::class,
-            // BookSeeder::class,
             GoogleBookSeeder::class,
+            BookSeeder::class,
             TransactionSeeder::class,
         ]);
 
